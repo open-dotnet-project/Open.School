d9df7110a4703020e6f4c17df31b81b845f26b3d
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-09-30 11:54:59 +0100

    application db context moved to data project

--- Open.School.Web/Program.cs
+++ Open.School.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Open.School.Web.Data;
+using Open.School.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
